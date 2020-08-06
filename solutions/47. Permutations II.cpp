8e38ec1f003195b930d6a9aa99ff7aa81bce7265
Shobhit Kashyap <36227863+shobhit32@users.noreply.github.com>
2020-08-06 09:19:15 +0530

    96 ms

---
+++ solutions/47. Permutations II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int len=nums.size();
+        if(len==0)
+            return ans;
+        sort(nums.begin(),nums.end());
+        do
+        {
+            ans.insert(ans.begin(),nums);
+        }while(next_permutation(nums.begin(),nums.end()));
+        return ans;
+    }
+};
