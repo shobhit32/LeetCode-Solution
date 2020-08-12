e538cfccd749b7e981542e82356eedea0a1c087e
Shobhit Kashyap <36227863+shobhit32@users.noreply.github.com>
2020-08-12 09:40:56 +0530

    N/A

--- solutions/39. Combination Sum.cpp
+++ solutions/39. Combination Sum.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    vector<int>v;
-    vector<vector<int>>ans;
-    void helper(vector<int>& candidates,int start,int target)
-    {
-        if(target<0)
-            return ;
-        if(target==0)
-        {
-            ans.push_back(v);
-            return ;
-        }
-         
-        while(start < candidates.size() && target-candidates[start]>=0)
-        { 
-             
-            v.push_back(candidates[start]);
-            helper(candidates,start,target-candidates[start]);
-            start++;
-            v.pop_back();
-        }  
-        
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        helper(candidates,0,target);
-        return ans;
-    }
+    vector<int>v;
+    vector<vector<int>>ans;
+    void helper(vector<int>& candidates,int start,int target)
+    {
+        if(target<0)
+            return ;
+        if(target==0)
+        {
+            ans.push_back(v);
+            return ;
+        }
+         
+        while(start < candidates.size() && target-candidates[start]>=0)
+        { 
+             
+            v.push_back(candidates[start]);
+            helper(candidates,start,target-candidates[start]);
+            start++;
+            v.pop_back();
+        }  
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        helper(candidates,0,target);
+        return ans;
+    }
 };
